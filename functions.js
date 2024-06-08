40d36a36ccb0d5eee89e1865036f9054b9ef9871
Samarth <a9756607821@gmail.com>
2024-06-08 21:00:49 +0530

    Added advanced search functionality and some other tweaks

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+import { isAdvanced } from "./getname.js"
+
 class TokenError extends Error {
     constructor(message){
         super(message)
@@ -7,19 +9,43 @@ class TokenError extends Error {
 
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 
-export function _d1(parsedDate){ 
+export function _f3(){
+    const btn_advanced = document.getElementById('advanced-search-toggle')
+    const div_advanced = document.getElementById('advanced-search-hidden')
+    
+    if (btn_advanced.checked == true){
+        div_advanced.style.display = 'block'
+        isAdvanced['advance'] = true
+    }
+    else {
+        div_advanced.style.display = 'none'
+        isAdvanced['advance'] = false
+    }
+    console.log(isAdvanced['advance'])
+}
+
+export function _d1(parsedDate, adv = false){ 
     const DISCORD_EPOCH = 1420070400000
-    const UNIXTIME = parseInt(new Date(parsedDate).valueOf()/1000)*1000
-    const DIFF = (UNIXTIME - DISCORD_EPOCH)
-    const EPOCH_TIME = DIFF * (2 ** 22)
-    return EPOCH_TIME
+    if (!adv) {
+        const UNIXTIME = parseInt(new Date(parsedDate).valueOf()/1000)*1000
+        const DIFF = (UNIXTIME - DISCORD_EPOCH)
+        const EPOCH_TIME = DIFF * (2 ** 22)
+        return EPOCH_TIME
+    }
+    else {
+        const UNIXTIME = new Date(parsedDate).valueOf()
+        const DIFF = (UNIXTIME - DISCORD_EPOCH)
+        const EPOCH_TIME = DIFF * (2 ** 22)
+        return EPOCH_TIME
+    }
 }
 
 export async function _f1(name, auth){
     const str_left = `${name} left`
     const label = document.getElementById('errormsg')
-    let offset = 0, api_error = false, retry_after
-    let url, joined_msg, timestamp, msg_id
+    let offset = 0, api_error = false, retry_after, entered_date, temp
+    let url, joined_msg, timestamp, msg_id, time_cur
+    let dd, mm, h, m, s
     while (true) {
         url = `https://discord.com/api/v9/guilds/524318544243195917/messages/search?content=${str_left}&offset=${offset}`
         console.log(`Fetching from URL: ${url.replace(" ", "%20")}`)
@@ -54,7 +80,7 @@ export async function _f1(name, auth){
                         const msg = response["messages"][i][0]["content"].split("\n")
                         for (let j = 0; j < msg.length; ++j) {
                             if (msg[j] == "```CSS") {
-                                let temp = msg[j+1].split("(")
+                                temp = msg[j+1].split("(")
                                 if ( (name.trim().length === temp[0].trim().length) && 
                                 name.toLowerCase() === temp[0].trim().toLowerCase() ) {
                                     joined_msg = msg[j+1]
@@ -63,7 +89,39 @@ export async function _f1(name, auth){
                                     msg_id = response["messages"][i][0]["id"]
                                     
                                     // To implement advanced search later here
-                                
+                                    
+                                    if (isAdvanced['advance'] == true){
+                                        dd = document.getElementById('dd').value.trim()
+                                        mm = document.getElementById('mm').value.trim()
+                                        const yy = document.getElementById('yy').value.trim()
+                                        h = document.getElementById('h').value.trim()
+                                        m = document.getElementById('m').value.trim()
+                                        s = document.getElementById('s').value.trim()
+                                        if (dd > 0 && dd < 10) { 
+                                            dd = `0${dd}`
+                                        }
+                                        if (mm > 0 && mm < 10) { 
+                                            mm = `0${mm}`
+                                        }
+                                        if (h > 0 && h < 10) { 
+                                            h = `0${h}`
+                                        }
+                                        if (m > 0 && m < 10) { 
+                                            m = `0${m}`
+                                        }
+                                        if (s > 0 && s < 10) { 
+                                            s = `0${s}`
+                                        }
+                                        time_cur = timestamp
+                                        entered_date = `${yy}-${mm}-${dd}T${h}:${m}:${s}`                                     
+                                        entered_date = _d1(entered_date, true)
+                                        time_cur = _d1(timestamp)
+                                        if ( entered_date >= time_cur ) {
+                                            throw new TypeError()
+                                        }
+
+                                        //throw new TypeError()
+                                    }
                                 }
                             }
                         }
@@ -157,5 +215,23 @@ export async function _f2(auth, max_id, playerid){
         if ( found ) { 
             return final_name
         }
+    }
+}
+
+export async function main(){
+    const btn = document.getElementById('submit')
+    btn.disabled = true
+    const NAME = document.getElementById('name').value.trim()
+    const AUTH = document.getElementById('token').value.trim()
+    try {
+        const response = await _f1(NAME, AUTH)
+        const DISCORD_EPOCH_TIME = _d1(response['timestamp'])
+        const f_name = await _f2(AUTH, DISCORD_EPOCH_TIME, response['playerid'])
+        alert(`${NAME} might be ${f_name}`)
+        btn.disabled = false
+    }
+    catch (error) { 
+        alert(error)
+        btn.disabled = false
     }
 }
